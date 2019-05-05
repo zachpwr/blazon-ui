6d0723727c8abc2919e6780fb3eba9848a1b3541
Zach Power <zachpwr@users.noreply.github.com>
2019-05-04 21:02:04 -0700

    Fix entry point, fix lint errors

--- src/theme.ts
+++ src/theme.ts
@@ -4,10 +4,10 @@ const theme: DefaultTheme = {
   borderRadius: '5px',
 
   colors: {
+    darkGray: '#2d3436',
     main: '#0984e3',
     secondary: '#dfe6e9',
     white: '#fff',
-    darkGray: '#2d3436',
   },
 };
 
