9733b69cc429bfb2086978f6e4ff32473ce78a77
Zach Power <zachpwr@users.noreply.github.com>
2019-05-18 23:04:04 -0700

    add alert component

---
+++ stories/alert.stories.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import Alert from '../src/alert';
+
+storiesOf('Alert', module).add('Default', () => (
+  <div>
+    <Alert>
+      <b>Fáilte!</b> Go raibh maith agat as clárú.
+    </Alert>
+    <Alert color="success">
+      <b>Samþykkt!</b> Umsóknin þín var staðfest og samþykkt.
+    </Alert>
+    <Alert color="warning">
+      <b>Rabhadh!</b> An do chuir thu eadar-theangachadh an teacsa seo?
+    </Alert>
+    <Alert color="error">
+      <b>Misslyckad!</b> Programmet kunde inte kompilera på grund av syntaxfel.
+    </Alert>
+    <Alert color="secondary">
+      <b>Интересный факт:</b> C++ компилируемый, статически типизированный язык программирования общего назначения.
+    </Alert>
+  </div>
+));
