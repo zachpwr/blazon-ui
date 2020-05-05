9ab0fc6766c565baf3a8544adad0689bb356321e
Zach Power <zachpwr@users.noreply.github.com>
2020-05-04 22:17:06 -0700

    fix(formsection): add aria orchestration logic

--- src/formSection/index.tsx
+++ src/formSection/index.tsx
@@ -1,21 +1,26 @@
+import { lighten } from 'polished';
 import * as React from 'react';
 import styled from 'styled-components';
-import { lighten } from 'polished';
 
 import { ITheme } from '../theme';
 
 export interface IFormSection {
+  children: (inputProps: {
+    id: string;
+    'aria-describedby': string | undefined;
+    'aria-labelledby': string | undefined;
+  }) => React.ReactElement;
   theme: ITheme;
   title: string;
   error?: string;
   info?: string;
   className?: string;
-  children?: React.ReactNode;
   required?: boolean;
 }
 
 interface IFormSectionTitle {
   theme: ITheme;
+  htmlFor: string;
   required?: boolean;
 }
 
@@ -27,7 +32,8 @@ interface IFormSectionInfo {
   theme: ITheme;
 }
 
-const SectionTitle = styled.div<IFormSectionTitle>`
+const SectionTitle = styled.label<IFormSectionTitle>`
+  display: block;
   font-size: 1em;
   margin-bottom: 5px;
   font-weight: 600;
@@ -60,17 +66,48 @@ const SectionInfo = styled.div<IFormSectionInfo>`
 
 SectionInfo.displayName = 'SectionInfo';
 
-const FormSection = styled((props: IFormSection) => (
-  <div className={props.className}>
-    <SectionTitle required={props.required}>{props.title}</SectionTitle>
-    {props.children}
-    {!props.error && props.info && <SectionInfo>{props.info}</SectionInfo>}
-    {props.error && <SectionError>{props.error}</SectionError>}
-  </div>
-))`
+class FormSection extends React.PureComponent<IFormSection> {
+  private static uniqueIdCounter = 0;
+
+  private uniqueId: string;
+
+  constructor(props: IFormSection) {
+    super(props);
+
+    this.uniqueId = `BlazonUI__FormSection__${FormSection.uniqueIdCounter}`;
+    FormSection.uniqueIdCounter++;
+  }
+
+  public render() {
+    const { className, children, error, info, title, required } = this.props;
+
+    // IDs for aria props
+    const inputId = `${this.uniqueId}__input`;
+    const labelId = `${this.uniqueId}__label`;
+    const infoId = `${this.uniqueId}__info`;
+    const errorId = `${this.uniqueId}__error`;
+
+    return (
+      <div className={className}>
+        <SectionTitle required={required} id={labelId} htmlFor={inputId}>
+          {title}
+        </SectionTitle>
+        {children({
+          'aria-describedby': (error && errorId) || (info && infoId) || undefined,
+          'aria-labelledby': labelId,
+          id: inputId,
+        })}
+        {!error && info && <SectionInfo id={infoId}>{info}</SectionInfo>}
+        {error && <SectionError id={errorId}>{error}</SectionError>}
+      </div>
+    );
+  }
+}
+
+const StyledFormSection = styled(FormSection)`
   margin-bottom: 20px;
 `;
 
-FormSection.displayName = 'FormSection';
+StyledFormSection.displayName = 'FormSection';
 
-export default FormSection;
+export default StyledFormSection;
