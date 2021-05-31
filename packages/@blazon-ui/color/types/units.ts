eeb0ef1d52f31c9fed6f076c776eed4bda4bdcd6
Zach Power <zachpwr@users.noreply.github.com>
2021-05-30 21:17:08 -0700

    add wcag2 utils

---
+++ packages/@blazon-ui/color/types/units.ts
@@ -0,0 +1,7 @@
+export type RGB = [ColorType: 'rgb', Red: number, Green: number, Blue: number];
+
+export type SRGB = [ColorType: 'srgb', Red: number, Green: number, Blue: number];
+
+export type RelativeLuminance = number;
+
+export type ContrastRatio = number;
